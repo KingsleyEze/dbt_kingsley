34962adb59f47cd6032c138ac3c8b9cacd59a208
Kingsley Eze <eze.o.kingsley@gmail.com>
2026-02-12 15:40:56 +0000

    saved purchase and sales cost margin

--- models/staging/sales_margin.sql
+++ models/staging/sales_margin.sql
@@ -1,5 +1,6 @@
 select t1.*,t2.purchase_price
-,t1.revenue-t2.purchase_price AS margin
+,t1.revenue- (t2.purchase_price*t1.quantity) AS margin
+,t1.quantity*purchase_price AS purchase_cost
 
 from {{ ref('stg_raw__sales') }} AS t1
 inner join {{ ref('stg_raw__product') }} AS t2
